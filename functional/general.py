292d7f3497f02f7c2dc47cffc25f0aff042d83fb
teethoe <ardithhowaiman@gmail.com>
2023-06-22 00:12:51 +0800

    show detections without bills

--- functional/general.py
+++ functional/general.py
@@ -695,4 +695,16 @@ def plotFeat(img, bill, eyes, tear_marks, start=(0, 0)):
                 cv2.line(img, tear_mark, bill, line_colours[i], 2)
                 cv2.circle(img, tear_mark, 4, (0, 150, 255), -1)
         cv2.circle(img, bill, 4, (255, 255, 0), -1)
+    else:
+        for i in range(2):
+            eye = eyes[i]
+            if eye:
+                eye = [round(start[i]+eye[i]) for i in range(2)]
+                cv2.circle(img, eye, 4, (0, 255, 255), -1)
+                cv2.circle(img, eye, 5, line_colours[i], 2)
+            tear_mark = tear_marks[i]
+            if tear_mark:
+                tear_mark = [round(start[i]+tear_mark[i]) for i in range(2)]
+                cv2.circle(img, tear_mark, 4, (0, 150, 255), -1)
+                cv2.circle(img, tear_mark, 5, line_colours[i], 2)
     return img
