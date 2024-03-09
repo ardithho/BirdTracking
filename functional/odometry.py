16bd9ed5dc2340b6420b97513f9e798a144af40c
Ardith Ho <35497665+teethoe@users.noreply.github.com>
2024-03-09 13:41:26 +0000

    Update odometry.py

--- functional/odometry.py
+++ functional/odometry.py
@@ -21,5 +21,5 @@ def estimateMotion(curr_frame, prev_frame):
     #     u1, v1 = kp1[m.queryIdx].pt
     #     u2, v2 = kp2[m.trainIdx].pt
     src_pts = np.float32([kp1[m.queryIdx].pt for m in filteredMatch]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.queryIdx].pt for m in filteredMatch]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in filteredMatch]).reshape(-1, 1, 2)
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
