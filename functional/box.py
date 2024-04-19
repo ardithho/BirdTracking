4ca02092cd3f443a6cd2ba04f65d7048f7a0f835
teethoe <ardithhowaiman@gmail.com>
2024-04-19 16:38:14 +0100

    fix yolov8 detect

---
+++ functional/box.py
@@ -0,0 +1,13 @@
+class Box:
+    def __init__(self, cls, conf, id, xywh, xywhn, xyxy, xyxyn):
+        self.cls = cls
+        self.conf = conf
+        self.id = id
+        self.xywh = xywh
+        self.xywhn = xywhn
+        self.xyxy = xyxy
+        self.xyxyn = xyxyn
+        self.xywhn = xywhn
+        self.xywhn = xywhn
+        self.xywhn = xywhn
+        self.xywhn = xywhn
--- yolov8/detect.py
+++ yolov8/detect.py
@@ -17,10 +17,10 @@ PROJECT_ROOT = Path(os.path.relpath(PROJECT_ROOT, Path.cwd()))  # relative
 
 class Detect:
     """
-    Detect bird features in image using our pre-trained model.
+    Detect bird head features in source using our pre-trained model.
     """
 
-    def __init__(self, model_path='yolov8/weights/head.pt'):
+    def __init__(self, model_path=ROOT / 'weights/full.pt'):
         self.model = YOLO(model_path)
 
     def predict(self, **kwargs):
@@ -31,7 +31,7 @@ class Detect:
 
 
 def run(
-        weights=ROOT / 'yolov5s.pt',  # model path or triton URL
+        weights=ROOT / 'weights/full.pt',  # model path or triton URL
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         conf=0.25,  # confidence threshold
         iou=0.7,  # NMS IOU threshold
@@ -45,14 +45,13 @@ def run(
         agnostic_nms=False,  # class-agnostic NMS
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         show=False,  # show results
-        save=True,  # save results to file
-        save_frames=False,  # save individual videos frames as images
+        no_save=False,  # do not save results to file
         save_txt=False,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
-        show_label=True,  # show labels
+        show_labels=True,  # show labels
         show_conf=True,  # show confidences
-        show_boxes=True,  # show boxes
+        boxes=True,  # show boxes
         line_width=3  # bounding box thickness (pixels)
 ):
     model = Detect(weights)
@@ -70,21 +69,20 @@ def run(
         agnostic_nms=agnostic_nms,
         classes=classes,
         show=show,
-        save=save,
-        save_frames=save_frames,
+        save=not no_save,
         save_txt=save_txt,
         save_conf=save_conf,
         save_crop=save_crop,
-        show_label=show_label,
+        show_labels=show_labels,
         show_conf=show_conf,
-        show_boxes=show_boxes,
+        boxes=boxes,
         line_width=line_width
     )
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov8n.pt', help='model path or triton URL')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'weights/full.pt', help='model path or triton URL')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--conf', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou', type=float, default=0.7, help='NMS IoU threshold')
@@ -98,17 +96,15 @@ def parse_opt():
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
     parser.add_argument('--show', action='store_true', help='show results')
-    parser.add_argument('--save', action='store_true', help='save results to file')
-    parser.add_argument('--save-frames', action='store_true', help='save individual video frames as images')
+    parser.add_argument('--no-save', action='store_true', help='do not save results to file')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--show-label', default=True, action='store_true', help='show labels')
+    parser.add_argument('--show-labels', default=True, action='store_true', help='show labels')
     parser.add_argument('--show-conf', default=True, action='store_true', help='show confidences')
-    parser.add_argument('--show-boxes', default=True, action='store_true', help='show bounding boxes')
+    parser.add_argument('--boxes', default=True, action='store_true', help='show bounding boxes')
     parser.add_argument('--line-width', default=3, type=int, help='bounding box thickness (pixels)')
     opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     return opt
 
 
