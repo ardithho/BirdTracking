f3e009c8844744b31185873e46aebc277a3b3f83
teethoe <ardithhowaiman@gmail.com>
2024-11-07 13:27:34 +0000

    update blender renderer

--- data/blender/marked.py
+++ data/blender/marked.py
@@ -1,6 +1,5 @@
 import bpy
 import os
-
 import cv2
 import yaml
 import numpy as np
@@ -19,24 +18,74 @@ os.makedirs(r_dir, exist_ok=True)
 scene = bpy.context.scene
 
 
+def extrinsic_mat(cam):
+    # bcam stands for blender camera
+    R_bcam2cv = Matrix(
+        ((1, 0,  0),
+         (0, -1, 0),
+         (0, 0, -1)))
+
+    # Transpose since the rotation is object rotation,
+    # and we want coordinate rotation
+    # R_world2bcam = cam.rotation_euler.to_matrix().transposed()
+    # T_world2bcam = -1*R_world2bcam * location
+
+    # Use matrix_world instead to account for all constraints
+    location, rotation = cam.matrix_world.decompose()[0:2]
+    R_world2bcam = rotation.to_matrix().transposed()
+
+    # Convert camera location to translation vector used in coordinate changes
+    # T_world2bcam = -1*R_world2bcam*cam.location
+    # Use location from matrix_world to account for constraints:
+    T_world2bcam = -1*R_world2bcam @ location
+
+    # Build the coordinate transform matrix from world to computer vision camera
+    # NOTE: Use * instead of @ here for older versions of Blender
+    R_world2cv = R_bcam2cv@R_world2bcam
+    T_world2cv = R_bcam2cv@T_world2bcam
+
+    # put into 3x4 matrix
+    RT = np.array([[*R_world2cv[0][:], T_world2cv[0]],
+                   [*R_world2cv[1][:], T_world2cv[1]],
+                   [*R_world2cv[2][:], T_world2cv[2]]])
+    return RT
+
+
 def camera_data(cam):
-    assert scene.render.resolution_percentage == 100
-    assert cam.sensor_fit != 'VERTICAL'
     f_in_mm = cam.lens
+    resolution_x_in_px = scene.render.resolution_x
+    resolution_y_in_px = scene.render.resolution_y
+    scale = scene.render.resolution_percentage / 100
     sensor_width_in_mm = cam.sensor_width
-    w = scene.render.resolution_x
-    h = scene.render.resolution_y
-    pixel_aspect = scene.render.pixel_aspect_y / scene.render.pixel_aspect_x
-    fx = f_in_mm / sensor_width_in_mm * w
-    fy = fx * pixel_aspect
-    # yes, shift_x is inverted
-    cx = w * (0.5 - cam.shift_x)
-    # and shift_y is still a percentage of width
-    cy = h * 0.5 + w * cam.shift_y
-    k = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
-    cam_loc = bpy.data.objects["Camera"].location
-    cam_rot = bpy.data.objects["Camera"].rotation_euler
-    ext = np.asarray(cam.matrix_world)[:3, :]
+    sensor_height_in_mm = cam.sensor_height
+    pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+    if (cam.sensor_fit == 'VERTICAL'):
+        # the sensor height is fixed (sensor fit is horizontal),
+        # the sensor width is effectively changed with the pixel aspect ratio
+        s_u = resolution_x_in_px * scale / sensor_width_in_mm / pixel_aspect_ratio
+        s_v = resolution_y_in_px * scale / sensor_height_in_mm
+    else:  # 'HORIZONTAL' and 'AUTO'
+        # the sensor width is fixed (sensor fit is horizontal),
+        # the sensor height is effectively changed with the pixel aspect ratio
+        pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+        s_u = resolution_x_in_px * scale / sensor_width_in_mm
+        s_v = resolution_y_in_px * scale * pixel_aspect_ratio / sensor_height_in_mm
+
+    # Parameters of intrinsic calibration matrix K
+    fx = f_in_mm * s_u
+    fy = f_in_mm * s_v
+    cx = resolution_x_in_px * scale / 2
+    cy = resolution_y_in_px * scale / 2
+    skew = 0  # only use rectangular pixels
+
+    k = np.array([[fx, skew, cx],
+                  [0, fy, cy],
+                  [0, 0, 1]])
+
+    # cam_loc = cam.location
+    # cam_rot = cam.rotation_euler
+    # ext = np.asarray(cam.matrix_world)[:3, :]
+    ext = extrinsic_mat(cam)
     return k, ext
 
 
