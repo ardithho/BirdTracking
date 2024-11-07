b6fea76b00b14592901c31cf03926473b6389fba
teethoe <ardithhowaiman@gmail.com>
2024-11-07 15:01:32 +0000

    add assemble for stereo

---
+++ data/blender/assemble_stereo.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+from pathlib import Path
+
+
+parent_dir = output_dir = Path(__file__).parent
+input_dir = os.path.join(parent_dir, 'marked')
+l_dir = os.path.join(input_dir, 'l')
+r_dir = os.path.join(input_dir, 'r')
+out_path = os.path.join(output_dir, 'marked.mp4')
+w, h = (1920*2, 1080)
+writer = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc(*'MPEG'), 10, (w, h))
+l_renders = [x for x in os.listdir(l_dir) if x.endswith('.png') or x.endswith('.jpg')]
+l_renders.sort()
+r_renders = [x for x in os.listdir(r_dir) if x.endswith('.png') or x.endswith('.jpg')]
+r_renders.sort()
+
+for l_render, r_render in zip(l_renders, r_renders):
+    l_render = cv2.imread(os.path.join(l_dir, l_render))
+    r_render = cv2.imread(os.path.join(r_dir, r_render))
+    render = cv2.hconcat([l_render, r_render])
+    writer.write(render)
+
+writer.release()
+cv2.destroyAllWindows()
+print('Rendered video saved at {}'.format(out_path))
