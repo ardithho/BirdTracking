cb0424f040ebd1eccab9f619369c3004b2849e25
teethoe <ardithhowaiman@gmail.com>
2024-11-28 13:28:28 +0000

    add odom params

--- utils/odometry.py
+++ utils/odometry.py
@@ -16,7 +16,8 @@ def extract_features(frame, mask=None, method='orb'):
     return detector.detectAndCompute(frame, mask)
 
 
-def find_matches(prev_frame, curr_frame, prev_mask=None, curr_mask=None, thresh=0.8, method='orb'):
+def find_matches(prev_frame, curr_frame,
+                 prev_mask=None, curr_mask=None, thresh=.8, method='orb'):
     kp1, des1 = extract_features(prev_frame, prev_mask, method)
     kp2, des2 = extract_features(curr_frame, curr_mask, method)
 
@@ -32,37 +33,47 @@ def find_matches(prev_frame, curr_frame, prev_mask=None, curr_mask=None, thresh=
         matches = flann.knnMatch(des1, des2, k=2)
         filtered = []
         for m, n in matches:
-            if m.distance < 0.7 * n.distance:
+            if m.distance < thresh * n.distance:
                 filtered.append(m)
     return filtered, kp1, kp2
 
 
-def find_matching_pts(prev_frame, curr_frame, prev_mask=None, curr_mask=None, thresh=0.8):
-    matches, kp1, kp2 = find_matches(prev_frame, curr_frame, prev_mask, curr_mask, thresh)
+def find_matching_pts(prev_frame, curr_frame,
+                      prev_mask=None, curr_mask=None, thresh=.8, method='orb'):
+    matches, kp1, kp2 = find_matches(prev_frame, curr_frame,
+                                     prev_mask, curr_mask, thresh, method)
 
     src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
     return src_pts, dst_pts
 
 
-def estimate_homography(prev_frame, curr_frame, prev_mask=None, curr_mask=None):
-    src_pts, dst_pts = find_matching_pts(curr_frame, prev_frame, curr_mask, prev_mask)
+def estimate_homography(prev_frame, curr_frame,
+                        prev_mask=None, curr_mask=None, thresh=.8, method='orb'):
+    src_pts, dst_pts = find_matching_pts(curr_frame, prev_frame,
+                                         curr_mask, prev_mask, thresh, method)
     return cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
 
-def estimate_essential_mat(prev_frame, curr_frame, prev_mask=None, curr_mask=None, K=None, dist=None):
+def estimate_essential_mat(prev_frame, curr_frame,
+                           prev_mask=None, curr_mask=None,
+                           K=None, dist=None, thresh=.8, method='orb'):
     # https://inst.eecs.berkeley.edu/~ee290t/fa19/lectures/lecture10-3-decomposing-F-matrix-into-Rotation-and-Translation.pdf
     # https://stackoverflow.com/questions/33906111/how-do-i-estimate-positions-of-two-cameras-in-opencv
-    src_pts, dst_pts = find_matching_pts(curr_frame, prev_frame, curr_mask, prev_mask)
+    src_pts, dst_pts = find_matching_pts(curr_frame, prev_frame,
+                                         curr_mask, prev_mask, thresh, method)
     if K is not None:
         return cv2.findEssentialMat(src_pts, dst_pts, K, dist, K, dist)
     return cv2.findEssentialMat(src_pts, dst_pts)
 
 
 # visual odometry
-def estimate_vio(prev_frame, curr_frame, prev_mask=None, curr_mask=None, K=None, dist=None, thresh=.8):
+def estimate_vio(prev_frame, curr_frame,
+                 prev_mask=None, curr_mask=None,
+                 K=None, dist=None, thresh=.8, method='orb'):
     # return: retval, R, t, mask
-    src_pts, dst_pts = find_matching_pts(prev_frame, curr_frame, prev_mask, curr_mask, thresh)
+    src_pts, dst_pts = find_matching_pts(prev_frame, curr_frame,
+                                         prev_mask, curr_mask, thresh, method)
     # return cv2.recoverPose(src_pts, dst_pts, K, dist, K, dist, threshold=thresh)
     if len(src_pts) > 0:
         return estimate_vio_pts(src_pts, dst_pts, K, dist)
@@ -125,10 +136,10 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture('../data/vid/fps120/K203_K238/GOPRO2/GH010039.MP4')
     if cap.isOpened():
         prev = cap.read()[1]
-    while cap.isOpened():
-        ret, curr = cap.read()
-        if ret:
-            print('Homography:', estimate_homography(prev, curr)[0])
-            print('Essential:', estimate_essential_mat(prev, curr)[0])
-            prev = curr
+        while cap.isOpened():
+            ret, curr = cap.read()
+            if ret:
+                print('Homography:', estimate_homography(prev, curr)[0])
+                print('Essential:', estimate_essential_mat(prev, curr)[0])
+                prev = curr
     cap.release()
