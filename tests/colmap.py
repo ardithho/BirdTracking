c7a4153fb1c279bc64e6977e341910d231f71470
teethoe <ardithhowaiman@gmail.com>
2025-01-27 17:59:32 +0000

    pnp with colmap

---
+++ tests/colmap.py
@@ -0,0 +1,108 @@
+import cv2
+import os
+import pycolmap
+
+import sys
+from pathlib import Path
+ROOT = Path(__file__).parent.parent
+sys.path.append(str(ROOT))
+
+from utils.general import RAD2DEG
+from utils.camera import Stereo
+from utils.structs import Bird, Birds
+from utils.sim import *
+from utils.reconstruct import get_head_feat_pts
+
+
+STRIDE = 1
+
+vid_path = ROOT / 'data/blender/marked_f.mp4'
+
+renders_dir = ROOT / 'data/blender/marked'
+cfg_path = os.path.join(renders_dir, 'cam.yaml')
+trans_path = os.path.join(renders_dir, 'transforms.txt')
+
+stereo = Stereo(path=cfg_path)
+
+with open(trans_path, 'r') as f:
+    lines = f.readlines()
+    transforms = [np.array(list(map(float, line.strip().split()[1:]))).reshape((4, 4)) for line in lines]
+
+# camL = pycolmap.Camera(
+#     model='OPENCV',
+#     width=stereo.camL.w,
+#     height=stereo.camL.h,
+#     params=(stereo.camL.K[0, 0], stereo.camL.K[1, 1],  # fx, fy
+#             stereo.camL.K[0, 2], stereo.camL.K[1, 2],  # cx, cy
+#             *stereo.camL.dist[:4]),  # dist: k1, k2, p1, p2
+# )
+
+cam = pycolmap.Camera(
+    model='SIMPLE_PINHOLE',
+    width=stereo.camL.w,
+    height=stereo.camL.h,
+    params=(stereo.camL.K[0, 0],  # focal length
+            stereo.camL.K[0, 2], stereo.camL.K[1, 2]),  # cx, cy
+)
+
+cap = cv2.VideoCapture(str(vid_path))
+birds = Birds()
+frame_no = 0
+sim.flip()
+T = np.eye(4)
+prev_T = T.copy()
+sim.update(T)
+gt = np.eye(4)
+cam_w, cam_h = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+dummy_head = Box(0, conf=[1.],
+                 xywh=np.array([[cam_w/2, cam_h/2, cam_w, cam_h]]),
+                 xywhn=np.array([[.5, .5, 1., 1.]]),
+                 xyxy=np.array([[0., 0., cam_w, cam_h]]),
+                 xyxyn=np.array([[0., 0., 1., 1.]]))
+while cap.isOpened():
+    for i in range(STRIDE):
+        if cap.isOpened():
+            _ = cap.grab()
+        else:
+            break
+    ret, frame = cap.retrieve()
+    if ret:
+        birds.update([Bird(dummy_head, extract_features(frame))], frame)
+        bird = birds['m'] if birds['m'] is not None else birds['f']
+        head_pts, feat_pts = get_head_feat_pts(bird)
+        pnp = pycolmap.estimate_and_refine_absolute_pose(feat_pts, head_pts, cam)
+        gt = transforms[frame_no] @ gt
+        if pnp is not None:
+            print(pnp['cam_from_world'])  # Rigid3d
+        #     r -= cv2.Rodrigues(ext[:3, :3])[0]
+        #     # r = r[[0, 2, 1]]
+        #     R, _ = cv2.Rodrigues(r)
+        #     # R = R.T
+        #     T[:3, :3] = R @ prev_T[:3, :3].T
+        #     print('es:', *np.rint(cv2.Rodrigues(T[:3, :3])[0]*RAD2DEG))
+        #     print('gt:', *np.rint(cv2.Rodrigues(transforms[frame_no][:3, :3])[0]*RAD2DEG))
+        #
+        #     print('esT:', *np.rint(r*RAD2DEG))
+        #     print('gtT:', *np.rint(cv2.Rodrigues(gt[:3, :3])[0]*RAD2DEG))
+        #     print('')
+        #     prev_T[:3, :3] = R
+        #     sim.update(T)
+        #
+        # cv2.imshow('frame', cv2.resize(birds.plot(frame), None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+        # out = cv2.vconcat([cv2.resize(birds.plot(frame), (w, h), interpolation=cv2.INTER_CUBIC),
+        #                    cv2.resize(sim.screen, (w, h), interpolation=cv2.INTER_CUBIC)])
+        #
+        # cv2.imshow('out', out)
+        # writer.write(out)
+
+        frame_no += 1
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+# writer.release()
+cv2.destroyAllWindows()
+sim.close()
+
