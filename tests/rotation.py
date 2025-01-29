2c94f53d4535e9581d650b6e4d6a33b45e7828fe
teethoe <ardithhowaiman@gmail.com>
2025-01-29 12:39:02 +0000

    add rotation test

---
+++ tests/rotation.py
@@ -0,0 +1,32 @@
+import cv2
+import yaml
+import os
+
+import sys
+from pathlib import Path
+ROOT = Path(__file__).parent.parent
+sys.path.append(str(ROOT))
+
+from utils.sim import *
+from utils.general import DEG2RAD
+
+
+renders_dir = ROOT / 'data/blender/marked'
+cfg_path = os.path.join(renders_dir, 'cam.yaml')
+with open(cfg_path, 'r') as f:
+    cfg = yaml.safe_load(f)
+    K = np.array(cfg['KF']).reshape(3, 3)
+    ext = np.array(cfg['extF']).reshape(3, 4)
+
+cam_rot = ext[:3, :3]
+cam_rot = cv2.Rodrigues(np.array([-90, 180, 0], dtype=np.float32)*DEG2RAD)[0]
+T = np.eye(4)
+r = np.array([90, 0, 0], dtype=np.float64)
+T[:3, :3] = cv2.Rodrigues(r * DEG2RAD)[0]
+sim.update(T)
+o3d.visualization.draw_geometries([sim.mesh],
+                                  front=cam_rot[:, 0],
+                                  lookat=cam_rot[:, 1],
+                                  up=cam_rot[:, 2],
+                                  zoom=1.
+                                  )
